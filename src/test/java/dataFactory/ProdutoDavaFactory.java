a55865c0dccc08c503ab8a864acb0034d60c9ab9
“Vitor” <vitorfagundes.7@gmail.com>
2021-09-12 23:12:34 -0300

    Initial commit

---
+++ src/test/java/dataFactory/ProdutoDavaFactory.java
@@ -0,0 +1,41 @@
+package dataFactory;
+
+import pojo.ComponentePojo;
+import pojo.ProdutoPojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoDavaFactory {
+
+    public static ProdutoPojo criarProdutoComumComOValorIgualA(double valor){
+        ProdutoPojo produto = new ProdutoPojo();
+        produto.setProdutoNome("PlayStation 5");
+        produto.setProdutoValor(valor);
+
+        List<String> cores = new ArrayList<>();
+        cores.add("preto");
+        cores.add("branco");
+
+        produto.setProdutoCores(cores);
+        produto.setProdutoUrlMock("");
+
+        List<ComponentePojo> componentes = new ArrayList<>();
+
+        ComponentePojo componente = new ComponentePojo();
+        componente.setComponenteNome("Controle");
+        componente.setComponenteQuantidade(3);
+
+        componentes.add(componente);
+
+        ComponentePojo segundoComponente = new ComponentePojo();
+        segundoComponente.setComponenteNome("Memory Card");
+        segundoComponente.setComponenteQuantidade(2);
+
+        componentes.add(segundoComponente);
+
+        produto.setComponentes(componentes);
+
+        return produto;
+    }
+}
